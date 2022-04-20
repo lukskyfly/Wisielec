69543d608a67864c53d193e3d9e2608b0cbd6b7e
lukskyfly <you@example.com>
2022-04-20 23:20:15 +0200

    add some reviev

--- szubienica.js
+++ szubienica.js
@@ -1,21 +1,16 @@
 window.onload = start;
 
-function kategorie ()
-{
+function kategorie (){
+
     losowanie();
     start();
 
 }
 
-let kraje = new Array(5);
-kraje[0]="Polska";
-kraje[1]="italia";
-kraje[2]="niemcy";
-kraje[3]="holandia";
-kraje[4]="irlandia";
+let kraje = ['polska','italia', 'niemcy', 'holandia', 'irlandia'];
 
-function losowanie()
-{
+function losowanie(){
+
     let numer=Math.floor(Math.random()*kraje.length);
     return kraje[numer];
 }
@@ -28,90 +23,56 @@ let yes = new Audio("yes.wav");
 let no = new Audio("no.wav");
 haslo = haslo.toUpperCase();
 
-for(i=0;i<dlugosc;i++)
-{
+for(i=0;i<dlugosc;i++){
+
     if(haslo.charAt(i)==" ") haslo1+= " ";
     else haslo1 += "-";
 }
  
-function wypiszhaslo()
-{
+function wypiszhaslo(){
+
     document.getElementById("plansza").innerHTML= haslo1;
 }
 
-let litery = new Array(35);
- 
-litery[0] = "A";
-litery[1] = "Ą";
-litery[2] = "B";
-litery[3] = "C";
-litery[4] = "Ć";
-litery[5] = "D";
-litery[6] = "E";
-litery[7] = "Ę";
-litery[8] = "F";
-litery[9] = "G";
-litery[10] = "H";
-litery[11] = "I";
-litery[12] = "J";
-litery[13] = "K";
-litery[14] = "L";
-litery[15] = "Ł";
-litery[16] = "M";
-litery[17] = "N";
-litery[18] = "Ń";
-litery[19] = "O";
-litery[20] = "Ó";
-litery[21] = "P";
-litery[22] = "Q";
-litery[23] = "R";
-litery[24] = "S";
-litery[25] = "Ś";
-litery[26] = "T";
-litery[27] = "U";
-litery[28] = "V";
-litery[29] = "W";
-litery[30] = "X";
-litery[31] = "Y";
-litery[32] = "Z";
-litery[33] = "Ż";
-litery[34] = "Ź";
- 
-function start()
-{
+let litery = ['A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ż', 'Ź'];
+  
+function start(){
+
     let tresc_diva = "";
      
-    for(i=0; i<=34;i++)
-    {
-        let element = "lit"+i;
-        tresc_diva += '<div class="litera" onclick="sprawdz('+ i +')"  id="' + element + '">' + litery[i] + '</div>';
-        if((i+1) % 7 ==0) tresc_diva=tresc_diva+'<div style="clear:both;"></div>';
+    for(i=0; i<=34;i++){
+
+        let element = `lit ${i}`;
+        tresc_diva += `<div class=litera onclick=sprawdz(${i}) id=${element}>${litery[i]}</div>`;
+        if((i+1) % 7 == 0) tresc_diva+='<div style="clear:both;"></div>';
     }
      
     document.getElementById("alfabet").innerHTML = tresc_diva;
     wypiszhaslo();
 }
  
-String.prototype.ustawZnak = function (miejsce,znak)
-{
-    if(miejsce>this.length-1) return this.toString();
-    else return this.substring(0,miejsce)+znak+this.substring(miejsce+1);
+ String.prototype.ustawZnak = function (miejsce,znak){
+
+    if(miejsce>this.length-1){
+        return this.toString()
+    }
+    else{
+        return this.substring(0,miejsce)+znak+this.substring(miejsce+1)
+    };
 }
  
-function sprawdz(nr)
-{
+function sprawdz(nr){
+
     let trafiona = false;
      
     for(i=0;i<dlugosc;i++)
     {
-        if(haslo.charAt(i)== litery[nr])
-        {
+        if(haslo.charAt(i)== litery[nr]){
             haslo1 = haslo1.ustawZnak(i,litery[nr]);
             trafiona=true;
         }
     }
-    if(trafiona==true)
-    {
+    if(trafiona==true){
         yes.play();
         let element = "lit"+nr;
         document.getElementById(element).style.background = "#003300";
@@ -120,8 +81,7 @@ function sprawdz(nr)
         document.getElementById(element).style.cursor = "default";
         wypiszhaslo();
     }
-    else
-    {
+    else{
         no.play();
         let element = "lit"+nr;
         document.getElementById(element).style.background = "#330000";
@@ -138,16 +98,18 @@ function sprawdz(nr)
     }
    
   //wygrana
-  if(haslo==haslo1)document.getElementById("alfabet").innerHTML = 'Tak jest! Podano prawidłowe hasło: '+haslo+'<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
-
+  if(haslo==haslo1){
+    document.getElementById("alfabet").innerHTML = 'Tak jest! Podano prawidłowe hasło: '+haslo+'<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+    }
    
   //przegrana
-  if(ile_skuch>=9)document.getElementById("alfabet").innerHTML = 'Przegrana! Prawidłowe hasło to: '+haslo+'<br/><br/><span class="reset" onclick="wybieszKategorie()">JESZCZE RAZ?</span>';
+  if(ile_skuch>=9){
+    document.getElementById("alfabet").innerHTML = 'Przegrana! Prawidłowe hasło to: '+haslo+'<br/><br/><span class="reset" onclick="wybieszKategorie()">JESZCZE RAZ?</span>';
+    }
 }
 let tresc_diva_kategorie = "";
 
-function wybieszKategorie ()
-{
-tresc_diva_kategorie += '<div id="alfabet">Wybierz kategorie hasła</div><br><br><span class="kategorie" onclick="kategorie()">"Państwa"</span><br/><span class="kategorie" >"Historia"</span> </br><span class="kategorie" >"Technologie"</span></br><span class="kategorie" >"Nauka"</span></br><span class="kategorie" >"Przysłowia"</span>'
+function wybieszKategorie (){
+tresc_diva_kategorie += '<div id="alfabet">Wybierz kategorie hasła</div><br><br><span class="kategorie" onclick="location.reload()">"Państwa"</span><br/><span class="kategorie" >"Historia"</span> </br><span class="kategorie" >"Technologie"</span></br><span class="kategorie" >"Nauka"</span></br><span class="kategorie" >"Przysłowia"</span>'
 document.getElementById("alfabet").innerHTML = tresc_diva_kategorie;
 }   
